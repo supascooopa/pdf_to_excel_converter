06bdab53ca4b6c46feca581b96eb84db36b2c41c
supascooopa <mnalptekin1@gmail.com>
2022-12-15 16:55:20 +0300

    add new_file_path function to tools.py

--- tools.py
+++ tools.py
@@ -1,5 +1,5 @@
 import itertools
-
+import datetime
 def onclick(event):
     """ Appending the x,y coordinates to a list when clicked """
     ix, iy = str(event.xdata), str(event.ydata)
@@ -12,3 +12,9 @@ def grouper(iterable, n, fillvalue=0):
     """ iterates and gives results with n times"""
     it = [iter(iterable)] * n
     return itertools.zip_longest(*it, fillvalue=fillvalue)
+
+def new_file_path(file_extension, filename=''):
+    now = datetime.datetime.now().strftime("%d-%m-%Y")
+    if filename:
+        return filename + now + file_extension
+    return now + file_extension
